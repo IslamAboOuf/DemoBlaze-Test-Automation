08c054a57600676b131006f06cbe53083e3c55c6
shams mohamed <shamsmohammed1110@gmail.com>
2026-06-14 22:07:58 +0300

    Edit productPage pom and product tests

--- src/main/java/Pages/ProductPage.java
+++ src/main/java/Pages/ProductPage.java
@@ -1,20 +1,16 @@
 package Pages;
-
 import Pages.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ProductPage extends BasePage {
     public ProductPage(WebDriver driver) { super(driver); }
 
-    public void addToCart() {
-        click(By.linkText("Add to cart"));
-        wait.until(ExpectedConditions.alertIsPresent());
-        getAlertTextAndAccept();
-    }
+    private final By addToCartBtn = By.linkText("Add to cart");
+    private final By productName = By.cssSelector(".name");
+    private final By priceContainer = By.cssSelector(".price-container");
 
-    public boolean isProductDetailsVisible() {
-        return wait.until(d -> driver.findElement(By.cssSelector(".name")).isDisplayed());
-    }
+    public void addToCart() { click(addToCartBtn); getAlertTextAndAccept(); }
+    public boolean isProductDetailsVisible() { return isElementDisplayed(productName); }
+    public boolean isPriceVisible() { return isElementDisplayed(priceContainer); }
 }
--- src/test/java/Test/ProductTest.java
+++ src/test/java/Test/ProductTest.java
@@ -3,32 +3,39 @@ package Test;
 import Base.BaseTest;
 import Pages.*;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ProductTest extends BaseTest {
 
+    private HomePage homePage;
+    private ProductPage productPage;
+    // 1. يجب تعريف cartPage هنا ليصبح متاحاً داخل الكلاس
+    private CartPage cartPage;
+
+    @BeforeMethod
+    public void setupProductTests() {
+        homePage = new HomePage(driver);
+        productPage = new ProductPage(driver);
+        // 2. يجب تهيئة الأوبجيكت هنا (حتى "يُبنى المنزل")
+        cartPage = new CartPage(driver);
+    }
+
     @Test
     public void TC_0025_verifyProductDetailsPageOpens() {
-        HomePage home = new HomePage(driver);
-        ProductPage product = new ProductPage(driver);
-
-        home.openProduct("Samsung galaxy s6");
-
-        Assert.assertTrue(product.isProductDetailsVisible(), "Product name should be visible");
-        // يمكن إضافة دالة checkPriceVisibility في ProductPage للتحقق من السعر
-        Assert.assertTrue(driver.findElement(org.openqa.selenium.By.cssSelector(".price-container")).isDisplayed(),
-                "Price container should be visible");
+        homePage.openProduct("Samsung galaxy s6");
+        Assert.assertTrue(productPage.isProductDetailsVisible(), "Product name should be visible");
+        Assert.assertTrue(productPage.isPriceVisible(), "Price container should be visible");
     }
 
     @Test
     public void TC_0026_verifyAddToCartFromProductPage() {
-        HomePage home = new HomePage(driver);
-        ProductPage product = new ProductPage(driver);
+        homePage.openProduct("Samsung galaxy s6");
+        productPage.addToCart();
+        homePage.openCart();
 
-        home.openProduct("Samsung galaxy s6");
-
-        product.addToCart();
-
-
+        // 3. الآن يمكنك استخدام cartPage (الذي تم تهيئته) بنجاح
+        Assert.assertTrue(cartPage.isProductDisplayed("Samsung galaxy s6"),
+                "Product should be visible in the cart after adding it.");
     }
 }
