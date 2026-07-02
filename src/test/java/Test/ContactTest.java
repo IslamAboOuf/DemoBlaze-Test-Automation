c307bcce15cf56d643a5ba4bc2ece97e2625a857
Islam Eldosoky <islam.eldosooky@gmail.com>
2026-07-02 08:15:44 +0300

    Re-enable known bug TCs to expose application defects

--- src/test/java/Test/ContactTest.java
+++ src/test/java/Test/ContactTest.java
@@ -1,7 +1,6 @@
 package Test;
 
 import Pages.ContactPage;
-import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,46 +23,41 @@ public class ContactTest extends Base.BaseTest {
 
     @Test()
     public void verifyContactUsFormSubmissionWithEmptyFields() {
-        throw new SkipException("Bug: Contact form submission with empty fields is not working as expected. Skipping this test until the bug is fixed.");
 
-//        contactPage.ContactForm("", "", "");
-//
-//        contactPage.verifyAlertMessage("Please fill out all fields.");
+        contactPage.ContactForm("", "", "");
+
+        contactPage.verifyAlertMessage("Please fill out all fields.");
     }
 
     @Test()
     public void verifyContactUsFormSubmissionWithEmptyContactName() {
-        throw new SkipException("Bug: Contact form submission with empty contact name is not working as expected. Skipping this test until the bug is fixed.");
 
-//        contactPage.ContactForm("test@test.com", "User1", "");
-//
-//        contactPage.verifyAlertMessage("Please fill out all fields.");
+        contactPage.ContactForm("test@test.com", "User1", "");
+
+        contactPage.verifyAlertMessage("Please fill out all fields.");
     }
 
     @Test()
     public void verifyContactUsFormSubmissionWithEmptyContactEmail() {
-        throw new SkipException("Bug: Contact form submission with empty contact email is not working as expected. Skipping this test until the bug is fixed.");
 
-//        contactPage.ContactForm("", "User1", "Hello");
-//
-//        contactPage.verifyAlertMessage("Please fill out all fields.");
+        contactPage.ContactForm("", "User1", "Hello");
+
+        contactPage.verifyAlertMessage("Please fill out all fields.");
     }
 
     @Test()
     public void verifyContactUsFormSubmissionWithEmptyMessage() {
-        throw new SkipException("Bug: Contact form submission with empty message is not working as expected. Skipping this test until the bug is fixed.");
 
-//        contactPage.ContactForm("test@test.com", "User1", "");
-//
-//        contactPage.verifyAlertMessage("Please fill out all fields.");
+        contactPage.ContactForm("test@test.com", "User1", "");
+
+        contactPage.verifyAlertMessage("Please fill out all fields.");
     }
 
     @Test()
     public void verifyContactUsFormSubmissionWithInvalidEmailFormat() {
-        throw new SkipException("Bug: Contact form submission with invalid email format is not working as expected. Skipping this test until the bug is fixed.");
 
-//        contactPage.ContactForm("testtest.com", "User1", "Hello");
-//
-//        contactPage.verifyAlertMessage("Invalid Email.");
+        contactPage.ContactForm("testtest.com", "User1", "Hello");
+
+        contactPage.verifyAlertMessage("Invalid Email.");
     }
 }
